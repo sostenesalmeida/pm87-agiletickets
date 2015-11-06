e879f9bc21ea9819dd9ccb8f472e59ba02e73c8f
sostenes <sostenesalmeida@mpf.mp.br>
2015-11-06 10:08:17 -0200

    codigo atualizado com testes junit

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -23,6 +23,60 @@ import org.joda.time.Weeks;
 @Entity
 public class Espetaculo {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result
+				+ ((estabelecimento == null) ? 0 : estabelecimento.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sessoes == null) ? 0 : sessoes.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Espetaculo other = (Espetaculo) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (estabelecimento == null) {
+			if (other.estabelecimento != null)
+				return false;
+		} else if (!estabelecimento.equals(other.estabelecimento))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sessoes == null) {
+			if (other.sessoes != null)
+				return false;
+		} else if (!sessoes.equals(other.sessoes))
+			return false;
+		if (tipo != other.tipo)
+			return false;
+		return true;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -102,17 +156,44 @@ public class Espetaculo {
      */
 	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
 		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
+		if (inicio.isAfter(fim)) {
+			throw new IllegalArgumentException("Data de inicio não pode ser maior do que a data fim");
+		}
+
+		List<Sessao> sessoes = new ArrayList<Sessao>();
+		if (Periodicidade.DIARIA == periodicidade) {
+			int dias = Days.daysBetween(inicio, fim).getDays();
+			for (int i = 0; i <= dias; i++) {
+				Sessao nova = new Sessao();
+				nova.setEspetaculo(this);
+				nova.setInicio(inicio.plusDays(i).toDateTime(horario));
+
+				sessoes.add(nova);
+			}
+		} else {
+			int semanas = Weeks.weeksBetween(inicio, fim).getWeeks();
+			for (int i = 0; i <= semanas; i++) {
+				Sessao nova = new Sessao();
+				nova.setEspetaculo(this);
+				nova.setInicio(inicio.plusWeeks(i).toDateTime(horario));
+
+				sessoes.add(nova);
+			}
+		}
+
+		return sessoes;
+		/*List<Sessao> l = new ArrayList<Sessao>();
 		if (periodicidade.equals(Periodicidade.DIARIA)) {
 			//diaria
 			Days d = Days.daysBetween(inicio, fim);
 			int dias = d.getDays();
-			List<Sessao> l = new ArrayList<Sessao>();
+			
 			if (dias >= 0 ) {
 				
-				for (int i = 0; i < dias-1; i++) {
+				for (int i = 0; i <= dias; i++) {
 					Sessao s = new Sessao();
 					
-					s.setEspetaculo(new Espetaculo());
+					s.setEspetaculo(this);
 					s.setPreco(new BigDecimal("1.0"));
 					s.setTotalIngressos(100);
 					s.setIngressosReservados(1);
@@ -132,7 +213,7 @@ public class Espetaculo {
 		}
 		
 		
-		return null;
+		return l;*/
 	}
 	
 	public boolean Vagas(int qtd, int min)
